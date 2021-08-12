8f85c6f1f465c77d2fe763814f0137b914c5194e
kae1506 <keshava.pprsd15@gmail.com>
2021-08-12 18:26:24 +0530

    adding testimonials and footer

--- mainpage.js
+++ mainpage.js
@@ -46,8 +46,7 @@ let btn = document.querySelector('.btn')
 let secondPanel = document.querySelector('.second-panel');
 let secondPanelText = secondPanel.querySelector('h1');
 
-let thirdPanel = document.querySelector('.third-panel');
-let thirdPanelText = thirdPanel.querySelector('h1');
+
 let walk = 50;
 let walkx = 75;
 
@@ -103,37 +102,34 @@ function secondPanelShadow(event){
         let _depth3 = `${50 - (_mouseX - _w) * 0.003}% ${50 - (_mouseY - _h) * 0.003}%`;
         var x = `${_depth3}, ${_depth2}, ${_depth1}`;
         secondPanel.style.backgroundPosition = x; 
+        console.log('owo')
 }
 
-function thirdPanelShadow(event2){
-    var {offsetWidth: width, offsetHeight: height} = thirdPanel;
-    var {offsetX: x, offsetY: y} = event2;
+// function thirdPanelShadow(event2){
+//     var {offsetWidth: width, offsetHeight: height} = thirdPanel;
+//     var {offsetX: x, offsetY: y} = event2;
 
-    if (this !== event2.target){
-        x = x + event2.target.offsetLeft;
-        y = y + event2.target.offsetTop;
-    }
+//     if (this !== event2.target){
+//         x = x + event2.target.offsetLeft;
+//         y = y + event2.target.offsetTop;
+//     }
 
-    var xWalk = Math.round((x / width * walkx) - (walkx / 2));
-    var yWalk = Math.round((y / height * walk) - (walk / 2));
+//     var xWalk = Math.round((x / width * walkx) - (walkx / 2));
+//     var yWalk = Math.round((y / height * walk) - (walk / 2));
 
-    thirdPanelText.style.margin = `${yWalk/2}px ${-xWalk/2}px ${-yWalk/2}px ${xWalk/2}px`;
-    let _w = window.innerWidth/2;
-    let _h = window.innerHeight/2;
-    let _mouseX = event2.clientX;
-    let _mouseY = event2.clientY;
-    let _depth1 = `${50 - (_mouseX - _w) * 0.0005}% ${50 - (_mouseY - _h) * 0.0005}%`;
-    let _depth2 = `${50 - (_mouseX - _w) * 0.001}% ${50 - (_mouseY - _h) * 0.001}%`;
-    let _depth3 = `${50 - (_mouseX - _w) * 0.003}% ${50 - (_mouseY - _h) * 0.003}%`;
-    var x = `${_depth3}, ${_depth2}, ${_depth1}`;
-    console.log(x);
-    thirdPanel.style.backgroundPosition = x; 
+//     thirdPanelText.style.margin = `${yWalk/2}px ${-xWalk/2}px ${-yWalk/2}px ${xWalk/2}px`;
+//     let _w = window.innerWidth/2;
+//     let _h = window.innerHeight/2;
+//     let _mouseX = event2.clientX;
+//     let _mouseY = event2.clientY;
+//     let _depth1 = `${50 - (_mouseX - _w) * 0.0005}% ${50 - (_mouseY - _h) * 0.0005}%`;
+//     let _depth2 = `${50 - (_mouseX - _w) * 0.001}% ${50 - (_mouseY - _h) * 0.001}%`;
+//     let _depth3 = `${50 - (_mouseX - _w) * 0.003}% ${50 - (_mouseY - _h) * 0.003}%`;
+//     var x = `${_depth3}, ${_depth2}, ${_depth1}`;
+//     console.log(x);
+//     thirdPanel.style.backgroundPosition = x; 
     
-}
-
-
-secondPanel.addEventListener('mousemove', secondPanelShadow);
-thirdPanel.addEventListener('mousemove', thirdPanelShadow);
+// }
 
 
 
@@ -151,16 +147,7 @@ observer_fbox1.observe(document.querySelector('.second-panel'));
 
 
 
-
-
-const fbox3 = document.querySelector('.third-panel');
-
-fbox3.classList.remove('third-panel-animation');
-const observer_fbox3 = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting && navbar_toggle) {
-            fbox3.classList.add('third-panel-animation');
-        }
-    });
-});
-observer_fbox3.observe(document.querySelector('.third-panel'));
+secondPanel.addEventListener('mousemove', secondPanelShadow);
+for(var i = 0; i < 100; i++){
+    console.log('aaa')
+}
